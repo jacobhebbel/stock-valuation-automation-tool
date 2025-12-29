20b0ef40458eb1e6b6fdf412ec78ac0e428b321e
jaocbhebbel <jacob.hebbel@gmail.com>
2025-12-29 12:27:23 -0500

    initial progress

---
+++ server.js
@@ -0,0 +1,66 @@
+// imports
+const express = require('express');
+const path = require('path');
+const app = express();
+const { stringToService } = require('utils');
+
+// middlewares
+app.use(express.json()); 
+
+
+// serve frontend
+app.use(express.static(path.join(__dirname, 'public')));
+
+// endpoints
+app.get('/health', async (req, res) => {
+
+    // will be handled by service objects
+    const factsetHealth = false; 
+    const edgarHealth = false;
+
+    // send a resposne indicating which services are on
+    return res.json({
+        serverHealth: true,
+        factsetHealth,
+        edgarHealth
+    });
+});
+
+app.get('/data/:stockTicker/:service', async (req, res) => {
+
+    let Service, ticker;
+    try {
+
+        if (!req.params.service || !req.params.stockTicker)
+            return res.status(400).json({ 'error': '1 or more path parameters missing' });
+
+        Service = stringToService(req.params.service);
+        ticker = req.params.stockTicker;
+    } catch (error) {
+
+        // The provided string didnt match one of the supported datasets
+        if (error instanceof InvalidServiceStringError)
+            return res.status(404).json({ 'error': 'the specified service does not match a supported service' });
+    }
+
+    // now we have the service, its time to query our dataset for the ticker 
+    // after getting the data, we return it in the required field: value format    
+
+    try {
+
+        // executes the query and returns a formatted json of field: value
+        const data = await Service.getTickerInfo(ticker);
+        return res.status(200).json({ data });
+    } catch (error) {
+
+        // log the error 
+        console.log('ERROR ENCOUNTERED\n\n%O\n\n\n', error);
+        
+        // stock ticker wasn't found in the service
+        if (error instanceof InvalidTickerStringError)
+            return res.status(404).json({ 'error': 'the provided ticker string wasn\'t found in the dataset' });
+        
+        // if we can't account for the error, then serve a general 500  
+        return res.status(500).json({ 'error': 'Internal Server Error '});
+    }
+});
