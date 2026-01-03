94b988ab7b68ff6c16c0e279f9715fed80481743
jaocbhebbel <jacob.hebbel@gmail.com>
2026-01-03 08:50:58 -0500

    increased robustness, gets better coverage on fields

--- services.js
+++ services.js
@@ -33,7 +33,7 @@ export class EdgarService extends TemplateService {
         console.log('calling fetch data on edgar service');
 
         const url = 'https://financialmodelingprep.com/stable/';
-        const endpoints = ['income-statement'];
+        const endpoints = ['income-statement', 'balance-sheet-statement', 'cash-flow-statement', 'key-metrics'];
         const params = {
             'symbol': ticker,
             'apikey': process.env.FMP_KEY
@@ -50,12 +50,30 @@ export class EdgarService extends TemplateService {
             const response = await axios.get(`${url}${endpoint}?${searchParams}`);
             
             // check the response is ok
-            if (response.status != 200) throw new Error(`Could not connect to SEC api`);        
-            console.log(response.data);
+            if (response.status != 200) throw new Error(`Could not connect to SEC api`);
             
-            // add the results
-            
-            Object.assign(allData, response.data);
+            // loops over all fields
+            const skipValues = ["symbol", "date", "fiscalYear", "period", "reportedCurrency"];
+            response.data.forEach(json => {
+                
+                // adds all entries of the json to allData, where each field has its metadata attached
+                for (const [key, value] of Object.entries(json)) {
+                    if (skipValues.includes(value))
+                        continue;
+
+                    if (!allData[key])
+                        allData[key] = [];
+
+                    // attaches metadata to each individual filing
+                    allData[key].push({
+                        "value": value,
+                        "date": json.date,
+                        "fiscalYear": json.fiscalYear,
+                        "period": json.period,
+                        "currency": json.reportedCurrency
+                    });
+                }
+            });
         }
         
         return allData;
@@ -63,20 +81,30 @@ export class EdgarService extends TemplateService {
 
     async formatData(data) {
         
+        // go through each field: alias combo
         let results = {};
-
-        // go through each field: alias combo for dcf
         Object.entries(mapping).forEach(([field, aliasList]) => {
-            
+
             console.log(`looking for field ${field} using alias list ${aliasList}`);
 
             // find the first alias that's in the edgar response object
             const matchingAlias = aliasList.find(alias => alias in data);
-            results[field] = data[matchingAlias];
+            
+            console.log(matchingAlias);
+            console.log(data[matchingAlias]);
+            
+            if (matchingAlias)
+                results[field] = {
+                    ...data[matchingAlias][0],
+                    givenAlias: field,
+                    reportedAs: matchingAlias
+                };
+            
 
             // ensure the field is populated. if not, throw an error
             if (!results[field])
-                throw new FieldNotFoundError('Could not find a filing label suitable for field ' + field);
+                // throw new FieldNotFoundError('Could not find a filing label suitable for field ' + field);
+                results[field] = null;
             else
                 console.log(`made entry ${field} : ${results[field]}`);
         });
